ee3d3c477c7750d1798918151d5f263fe23cf02d
Jim Veneskey <github@strifecrafter.com>
2015-03-25 12:31:05 -0400

    Initial Commit

---
+++ UITableViewController Challenge/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge
+//
+//  Created by jim Veneskey on 3/25/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge
+//
+//  Created by jim Veneskey on 3/25/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
